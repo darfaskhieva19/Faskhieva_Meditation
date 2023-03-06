bfbeb016e3c6ca149ec6c1a7dcc7efadd02b3946
unknown <@ngknn.local>
2023-03-06 14:59:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/meditation/ScreenSplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.meditation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class ScreenSplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGHT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(ScreenSplashActivity.this,MainActivity.class);
+                ScreenSplashActivity.this.startActivity(mainIntent);
+                ScreenSplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+}
